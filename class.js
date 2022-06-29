7f171fa8d07529f976dd8b5fdfe4d803af835bae
Andrew Martins <andrew_omartins@yahoo.com>
2022-06-28 21:24:55 -0400

    created a class and will now start method chaining

---
+++ class.js
@@ -0,0 +1,32 @@
+class Car {
+    constructor(color, model, make){
+        this.color = color
+        this.model = model
+        this.make = make
+    }
+    carStart(){
+        console.log("the car is on")
+        return this
+    }
+    accelerate(){
+        console.log("the car is moving my dude")
+        return this
+    }
+    carStop(){
+        console.log("the car is no moving my dude")
+        return this
+    }
+    carOff(){
+        console.log("the car is off my dude")
+    }
+    
+}
+
+const carOne = new Car('red', 'enzo', 'ferrari')
+const carTwo = new Car('green', 'murcielago', 'lamborghini')
+const carThree = new Car('pink', '911gt', 'porsche')
+
+
+console.log(carOne)
+
+
